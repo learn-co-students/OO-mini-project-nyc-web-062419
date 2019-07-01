4d13969a77489a01bb701d2fdaea77c594a30608
IanGrubb <yangrubb@gmail.com>
2019-07-01 16:06:51 -0400

    Worked with @adriennem76

--- app/models/Ingredient.rb
+++ app/models/Ingredient.rb
@@ -1,1 +1,34 @@
+class Ingredient
 
+    @@all = []
+
+    attr_reader :name
+    
+    def initialize(name)
+        @name = name
+        Ingredient.all << self        
+    end
+
+    def self.all
+        @@all
+    end
+
+    def allergies
+        Allergy.all.select {|allergy| allergy.ingredient == self}
+    end
+
+    def allergic_users
+        self.allergies.collect {|allergy| allergy.user}.uniq
+    end
+
+    def self.most_common_allergen
+        candidate = self.all[0]
+        self.all.each do |ingredient| 
+            if ingredient.allergic_users.length > candidate.allergic_users.length
+                candidate = ingredient
+            end
+        end        
+        candidate
+    end
+
+end
---
+++ app/models/RecipeIngredient.rb
@@ -0,0 +1,17 @@
+class RecipeIngredient
+
+    @@all = []
+
+    attr_reader :recipe, :ingredient
+
+    def initialize (recipe, ingredient)
+        @recipe = recipe
+        @ingredient = ingredient
+        RecipeIngredient.all << self      
+    end
+
+    def self.all
+        @@all
+    end
+
+end
